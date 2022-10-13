9222315a91af7db0d137355828b1eaa1f1f9fcac
Santi Galera <santiagogalera@gmail.com>
2022-10-13 15:04:02 -0300

    agregando librerias

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//DOM
 const carritoBtn = document.getElementById("carrito");
 const modal = document.getElementById("modal_overlay");
 const cerrarModal = document.querySelector(".modal_close");
@@ -7,13 +8,30 @@ const span = document.querySelector(".contador");
 const cards = document.querySelector(".cards");
 const carritoCards = document.getElementById("cards_carrito");
 const precioFinal = document.querySelector(".total");
+const seachBar = document.querySelector(".search");
 
-vaciarCarrito.addEventListener("click", () => {
+//Evento para vaciar el carrito
+vaciarCarrito.addEventListener("", () => {
   carrito.length = 0;
   actualizarCarrito();
   localStorage.removeItem("carrito");
+  Toastify({
+    text: "Se vacio el carrito!",
+    className: "toast_red",
+    duration: 2500,
+  }).showToast();
 });
 
+// Search
+seachBar.addEventListener("keyup", (e) => {
+  let filteredProductos = productos.filter((product) => {
+    return product.nombre.match(e.target.value); //* busca en ccada producto, esta searchbar en KEY SENSITIVE
+  });
+  cards.innerHTML = null; //* Borra todas las cartas
+  renderCards(filteredProductos); //* Llama a la funcion render con los productos filtrados
+});
+
+//Modal
 carritoBtn.addEventListener("click", (e) => {
   e.preventDefault();
   modal.classList.add("modal_show");
@@ -23,39 +41,82 @@ cerrarModal.addEventListener("click", (e) => {
   modal.classList.remove("modal_show");
 });
 
+//Declaracion de variables
+let carrito = [];
+let productos = [];
 
-const carrito = [];
-const productos = [];
-
+//Construccion de objetos
 class producto {
-  constructor(id, nombre, precio, stock) {
+  constructor(id, nombre, precio, stock, img) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
-    this.img = img
+    this.img = img;
   }
   sumarIva() {
     return (this.precio = this.precio * 1), 21;
   }
-  vendido() {
-    this.stock = this.stock - 1;
-  }
 }
 
-const producto1 = new producto(1, "Buzo Gvng", 12599, 2);
-productos.push(producto1);
-const producto2 = new producto(2, "Jogger", 7000, 1);
-productos.push(producto2);
-const producto3 = new producto(3, "Chomba Dc", 5790, 3);
-productos.push(producto3);
-const producto4 = new producto(4, "Campera KOTK", 13599, 2);
-productos.push(producto4);
-const producto5 = new producto(5, "Gorro", 4000, 4);
-productos.push(producto5);
-const producto6 = new producto(6, "Riñonera", 3000, 5);
-productos.push(producto6);
+const producto1 = new producto(
+  1,
+  "camiseta titular",
+  15000,
+  3,
+  "img/camiseta titular.webp",
+);
+const producto2 = new producto(
+  2,
+  "camiseta suplente",
+  15000,
+  4,
+  "img/camiseta suplente.webp",
+);
+const producto3 = new producto(3, "short titular", 9000, 6, "img/short.webp");
+const producto4 = new producto(
+  4,
+  "short suplente",
+  9000,
+  2,
+  "img/short suplente.webp",
+);
+const producto5 = new producto(
+  5,
+  "camiseta de arquero",
+  15000,
+  2,
+  "img/camiseta arquero.webp",
+);
+const producto6 = new producto(6, "gorra", 3000, 3, "img/gorra.webp");
+const producto7 = new producto(7, "medias titular", 3500, 4, "img/medias.webp");
+const producto8 = new producto(
+  8,
+  "medias suplente",
+  3500,
+  2,
+  "img/medias sup.webp",
+);
+const producto9 = new producto(
+  9,
+  "short arquero",
+  9500,
+  5,
+  "img/short arquero.webp",
+);
+productos.push(
+  producto1,
+  producto2,
+  producto3,
+  producto4,
+  producto5,
+  producto6,
+  producto7,
+  producto8,
+  producto9,
+);
 
+//Obtengo el localstorage al inciar
 document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("carrito")) {
     carrito = JSON.parse(localStorage.getItem("carrito"));
@@ -63,6 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+//Funciones
 function agregarAlCarrito(producto) {
   let buscarProducto = carrito.find((item) => item.id === producto.id);
   if (buscarProducto !== undefined) {
@@ -70,10 +132,7 @@ function agregarAlCarrito(producto) {
     buscarProducto.cantidad = buscarProducto.cantidad + 1;
   } else {
     carrito.push({
-      id: producto.id,
-      nombre: producto.nombre,
-      precio: producto.precio,
-      img: producto.img,
+      ...producto,
       cantidad: 1,
     });
   }
@@ -83,13 +142,14 @@ function agregarAlCarrito(producto) {
 function actualizarCarrito() {
   carritoCards.innerHTML = "";
   carrito.forEach((producto) => {
+    const { id, nombre, precio, img } = producto;
     let div = document.createElement("div");
     div.innerHTML = `
-      <img src="${producto.img}">
-      <h3>${producto.nombre}</h3>
+      <img src="${img}">
+      <h3>${nombre}</h3>
       <p>Cantidad:${producto.cantidad}</p>
-      <p>$${producto.precio}</p>
-      <button id="eliminar${producto.id}" class="btn eliminar">Eliminar</button>
+      <p>$${precio}</p>
+      <button id="eliminar${id}" class="btn eliminar">Eliminar</button>
       `;
     localStorage.setItem("carrito", JSON.stringify(carrito));
     carritoCards.append(div);
@@ -107,22 +167,38 @@ function eliminarDelCarrito(producto) {
   let indice = carrito.indexOf(buscado);
   carrito.splice(indice, 1);
   actualizarCarrito();
+  Toastify({
+    text: "Se elimino el producto del carrito!",
+    className: "toast_red",
+    duration: 2500,
+  }).showToast();
 }
 
-productos.forEach((producto) => {
-  let div = document.createElement("div");
-
-  div.innerHTML = `
-  <img src="${producto.img}">
-  <h3>${producto.nombre}</h3>
-  <p>$${producto.precio}</p>
-  <button id=${producto.id} class="btn">Agregar al Carrito</button>
+//DOM renderizo productos
+function renderCards(productos) {
+  //* Le llegan los productos por parametros
+  productos.forEach(({ id, nombre, precio, img }) => {
+    //* destructuring dentro de la arrow
+    let div = document.createElement("div");
+    div.innerHTML = `
+  <img src="${img}">
+  <h3>${nombre}</h3>
+  <p>$${precio}</p>
+  <button id=${id} class="btn">Agregar al Carrito</button>
   `;
-  div.className = "card";
-  cards.append(div);
+    div.className = "card";
+    cards.append(div);
 
-  const boton = document.getElementById(producto.id);
-  boton.addEventListener("click", (e) => {
-    agregarAlCarrito(producto);
+    const boton = document.getElementById(id);
+    boton.addEventListener("click", (e) => {
+      agregarAlCarrito({ id, nombre, precio, img });
+      Toastify({
+        text: "Se agrego el producto al carrito!",
+        className: "toast",
+        duration: 2500,
+      }).showToast();
+    });
   });
-});
+}
+
+renderCards(productos); //* LLamo en el inicio con los productos
